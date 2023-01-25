63b72e5b22f3fc3e233fa60e0508c09b30bf9f59
chenannchi <anchichenusa@gmail.com>
2023-01-25 22:15:34 +0800

    Add a loading page

---
+++ src/pages/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import styles from './Loading.module.css'
+
+const Loading = () => {
+  return (
+    <main className={styles.container}>
+      <h1>Loading</h1>
+    </main>
+  )
+}
+
+export default Loading
