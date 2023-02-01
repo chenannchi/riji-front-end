848bb851395a78743004afa040fae1bacafac4db
chenannchi <anchichenusa@gmail.com>
2023-02-01 17:09:30 -0500

    set up diary details page

--- src/services/diaryService.js
+++ src/services/diaryService.js
@@ -13,4 +13,18 @@ const index = async () => {
   }
 }
 
-export { index }
+const show = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export {
+  index,
+  show,
+}
