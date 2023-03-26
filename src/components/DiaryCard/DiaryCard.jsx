d162edb7c51dfa51cf23919c0e66accdb672a9bc
chenannchi <anchichenusa@gmail.com>
2023-03-26 15:05:27 -0400

    modify the layout of diarybook detail

--- src/components/DiaryCard/DiaryCard.jsx
+++ src/components/DiaryCard/DiaryCard.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom"
 import styles from './DiaryCard.module.css'
 const DiaryCard = ({ diary }) => {
   return (
-    <Link to={`/diaries/${diary._id}`}>
-      <div className={styles.container}>
+    <div className={styles.container}>
+      <Link to={`/diaries/${diary._id}`}>
         <div className={styles.diary_preview}>
           <div>{diary.title}</div>
           {/* <p>{diary.content}</p> */}
-          <div>{diary.createdAt.slice(0,10)}</div>
+          <div>{diary.createdAt.slice(0, 10)}</div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
 
--- src/pages/DiarybookDetails/DiarybookDetails.jsx
+++ src/pages/DiarybookDetails/DiarybookDetails.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import { useParams, Link } from "react-router-dom"
 import styles from './DiarybookDetails.module.css'
 import DiaryCard from "../../components/DiaryCard/DiaryCard"
+import { AiOutlineEdit, AiOutlineDelete } from "react-icons/ai"
 
 // Services
 import * as diarybookService from '../../services/diarybookService'
@@ -27,41 +28,51 @@ const DiarybookDetails = (props) => {
   if (!diarybook) return <Loading />
 
   return (
-    <main className={styles.container}>
-      {/* <article> */}
-      <header>
-        <h1>{diarybook.name}</h1>
-        <div className={styles.btns}>
-          {diarybook.owner.filter(member => member._id === props.user.profile) &&
-            <div>
-              <Link to={`/diarybooks/${id}/edit`} state={diarybook}>Edit</Link>
-              <div onClick={() => props.handleDeleteDiarybook(id)}>Delete</div>
+    <main className={styles.pageContainer}>
+      <div className={styles.container}>
+        <div id={styles.left} className={styles.area}>
+          <div>
+            <div id={styles.name}>
+              <h2>{diarybook.name}</h2>
+              <div className={styles.btns}>
+                {diarybook.owner.filter(member => member._id === props.user.profile) &&
+                  <div>
+                    <Link to={`/diarybooks/${id}/edit`} state={diarybook}><AiOutlineEdit /></Link>
+                    <div onClick={() => props.handleDeleteDiarybook(id)}><AiOutlineDelete /></div>
+                  </div>
+                }
+              </div>
             </div>
-          }
-        </div>
-        <div id={styles.owner}>
-          <div>Owned by</div>
-          {diarybook.owner.map(member => (
-            <div key={member._id}>
-              <div>{member.name}</div>
-              {member !== diarybook.owner[diarybook.owner.length - 1] ? <span>,</span> : null}
+            <div>{diarybook.description}</div>
+            <div id={styles.owner}>
+              <div>Owned by&nbsp;</div>
+              {diarybook.owner.map(member => (
+                <div key={member._id}>
+                  <div><b>{member.name === props.user.name ? "You" : `${member.name}`}</b></div>
+                  {member !== diarybook.owner[diarybook.owner.length - 1] ? <span>,</span> : null}
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
+          
+          <section>
+            <h2>Diaries</h2>
+            <div className={styles.diaryContainer}>
+              {
+                diarybook.diaries.length ?
+                  diarybook.diaries.map(diary => (
+                    <DiaryCard key={diary._id} diary={diary} />
+                  ))
+                  :
+                  <div>No Diaries Yet!</div>
+              }
+            </div>
+          </section>
         </div>
-        
-      </header>
-
-
-      <p>{diarybook.description}</p>
-      <section>
-        <h1>Diaries</h1>
-        <div className={styles.diaryContainer}>
-          {diarybook.diaries.map(diary => (
-            // <Link to={`/diarybooks/${id}/diaries/${diary._id}`} key={diary._id}>{diary.title}</Link>
-            <DiaryCard key={diary._id} diary={diary} />
-          ))}
+        <div id={styles.left} className={styles.area}>
+          Diary Preview
         </div>
-      </section>
+      </div>
     </main>
   )
 }
